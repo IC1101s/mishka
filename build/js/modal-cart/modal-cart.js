a8a102d68a661f705d9e75aab319ab9cfd4cefd3
IC1101 <stas-s2000@mail.ru>
2020-05-08 18:37:33 +0300

    Добавил всю папку build/ в проект (c 1 страницей)

---
+++ build/js/modal-cart/modal-cart.js
@@ -0,0 +1,31 @@
+var recommendationButton = document.querySelector(".recommendation__button");
+var modalCart = document.querySelector(".modal-cart");
+var modalCartButton = document.querySelector(".modal-cart__button");
+var modalOverlay = document.querySelector(".modal-overlay");
+var modalInput = document.querySelector(".modal-cart__input");
+
+recommendationButton.addEventListener("click", function () {
+	modalCart.classList.add("modal-cart--visible");
+	modalOverlay.classList.add("modal-overlay--visible");
+	modalInput.focus();
+});
+
+modalOverlay.addEventListener("click", function () {
+	modalCart.classList.remove("modal-cart--visible");
+	modalOverlay.classList.remove("modal-overlay--visible");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		modalCart.classList.remove("modal-cart--visible");
+		modalOverlay.classList.remove("modal-overlay--visible");
+	}
+});
+
+// Временно!!!!!!!
+modalCartButton.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modalCart.classList.remove("modal-cart--visible");
+	modalOverlay.classList.remove("modal-overlay--visible");
+});
